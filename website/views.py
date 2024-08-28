eef706c482bf055f3ce87f91017cf37d21ae1af3
Juan Ospina <zuleta2411@hotmail.com>
2024-08-28 17:01:43 -0500

    individual records and delete function added

--- website/views.py
+++ website/views.py
@@ -45,3 +45,26 @@ def register_user(request):
         form = SignUpForm()
         return render(request, 'register.html', {'form': form})
     return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.success(
+            request, "You must be logged in to that page")
+        return redirect('home')
+
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(
+            request, "Information deleted successfully")
+        return redirect('home')
+    else:
+        messages.success(
+            request, "You must be logged in to the page")
+        return redirect('home')
